f4101b6e6895f7d28c1e418336518dd5870d1dc1
NicollPinzon <113453727+NicollPinzon@users.noreply.github.com>
2023-06-02 18:42:38 -0400

    :)

--- Proyectito/src/proyectito/Main.java
+++ Proyectito/src/proyectito/Main.java
@@ -20,57 +20,57 @@ public class Main {
     public static void main(String[] args) {
 
         Bienvenido20 bn = new Bienvenido20();
-        TxtManager txt = new TxtManager();
-        Grafo grafito = txt.readText();
-        System.setProperty("org.graphstream.ui", "swing");
-        System.out.println("SIUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUU");
-
-        Graph graph = new SingleGraph("Proyecto");
-        graph.clear();
-        int id = 0;
-
-        for (int i = 0; i < grafito.getTotalusers(); i++) {
-            graph.addNode(Integer.toString(grafito.getUserList()[i].getId()));
-        }
-        for (int i = 0; i < grafito.getTotalusers(); i++) {
-
-            NodoLista nodo = grafito.getUserList()[i].getHead();
-            while (nodo != null) {
-                int npb = 0;
-                if (id == 0) {
-                    graph.addEdge(Integer.toString(id), Integer.toString(grafito.getUserList()[i].getId()), Integer.toString(nodo.getId()));
-                    id++;
-                } else {
-                    Object Node5 = Integer.toString(grafito.getUserList()[i].getId());
-                    Object Node7 = Integer.toString(nodo.getId());
-                    while (npb < id - 1) {
-                        if (graph.getEdge(Integer.toString(npb)).getNode1() == Node5 || graph.getEdge(Integer.toString(npb)).getNode0() == Node7) {
-                            if (graph.getEdge(Integer.toString(npb)).getNode0() == Node5 || graph.getEdge(Integer.toString(npb)).getNode1() == Node7) {
-                                graph.addEdge(Integer.toString(id), Integer.toString(grafito.getUserList()[i].getId()), Integer.toString(nodo.getId()));
-                                id++;
-                            }
-
-                        }
-
-                        npb++;
-
-                    }
-                }
-                nodo = (NodoLista) nodo.getNext();
-            }
-
 //        TxtManager txt = new TxtManager();
-//         Grafo grafito =txt.readText();
-//         Mostrar_Grafo np = new Mostrar_Grafo();
-//         np.gafo();
-//        Bienvenido20 bn = new Bienvenido20();
-//    grafo.addConnection(121, 254, 7);
-//        grafo.printGrafo();
-//    grafo.deleteConnection(121, 254);
-//        System.out.println("--------");
-//    grafo.printGrafo();
-        }
-        new Mostrar_Grafo();
+//        Grafo grafito = txt.readText();
+//        System.setProperty("org.graphstream.ui", "swing");
+//        System.out.println("SIUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUU");
+//
+//        Graph graph = new SingleGraph("Proyecto");
+//        graph.clear();
+//        int id = 0;
+//
+//        for (int i = 0; i < grafito.getTotalusers(); i++) {
+//            graph.addNode(Integer.toString(grafito.getUserList()[i].getId()));
+//        }
+//        for (int i = 0; i < grafito.getTotalusers(); i++) {
+//
+//            NodoLista nodo = grafito.getUserList()[i].getHead();
+//            while (nodo != null) {
+//                int npb = 0;
+//                if (id == 0) {
+//                    graph.addEdge(Integer.toString(id), Integer.toString(grafito.getUserList()[i].getId()), Integer.toString(nodo.getId()));
+//                    id++;
+//                } else {
+//                    Object Node5 = Integer.toString(grafito.getUserList()[i].getId());
+//                    Object Node7 = Integer.toString(nodo.getId());
+//                    while (npb < id - 1) {
+//                        if (graph.getEdge(Integer.toString(npb)).getNode1() == Node5 || graph.getEdge(Integer.toString(npb)).getNode0() == Node7) {
+//                            if (graph.getEdge(Integer.toString(npb)).getNode0() == Node5 || graph.getEdge(Integer.toString(npb)).getNode1() == Node7) {
+//                                graph.addEdge(Integer.toString(id), Integer.toString(grafito.getUserList()[i].getId()), Integer.toString(nodo.getId()));
+//                                id++;
+//                            }
+//
+//                        }
+//
+//                        npb++;
+//
+//                    }
+//                }
+//                nodo = (NodoLista) nodo.getNext();
+//            }
+//
+////        TxtManager txt = new TxtManager();
+////         Grafo grafito =txt.readText();
+////         Mostrar_Grafo np = new Mostrar_Grafo();
+////         np.gafo();
+////        Bienvenido20 bn = new Bienvenido20();
+////    grafo.addConnection(121, 254, 7);
+////        grafo.printGrafo();
+////    grafo.deleteConnection(121, 254);
+////        System.out.println("--------");
+////    grafo.printGrafo();
+//        }
+//        new Mostrar_Grafo();
     }
 
 //        grafo.emptyGrafo();
